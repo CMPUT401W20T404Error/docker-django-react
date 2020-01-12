061a6acec404c4b6ce5011a02b4163fbdac4ffe0
Jawad Rizvi <jawadrizvi@Jawads-MacBook-Air.local>
2020-01-12 12:00:15 -0700

    yea it worked

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,26 @@
 import React from "react";
 import BubbleChart from '@weknow/react-bubble-chart-d3';
 import "./App.css";
-// import data from './data.js';
+import data from './data.js';
 
-const data = [
-  { label: 'CRM', value: 1 },
-  { label: 'API', value: 1 },
-  { label: 'Data', value: 1 },
-  { label: 'Commerce', value: 1 },
-  { label: 'AI', value: 3 },
-  { label: 'Management', value: 5 },
-  { label: 'Testing', value: 6 },
-  { label: 'Mobile', value: 9 },
-  { label: 'Conversion', value: 9 },
-  { label: 'Misc', value: 21 },
-  { label: 'Databases', value: 22 },
-  { label: 'DevOps', value: 22 },
-  { label: 'Javascript', value: 23 },
-  { label: 'Languages / Frameworks', value: 25 },
-  { label: 'Front End', value: 26 },
-  { label: 'Content', value: 26 },
-]
+// const data = [
+//   { label: 'CRM', value: 1 },
+//   { label: 'API', value: 1 },
+//   { label: 'Data', value: 1 },
+//   { label: 'Commerce', value: 1 },
+//   { label: 'AI', value: 3 },
+//   { label: 'Management', value: 5 },
+//   { label: 'Testing', value: 6 },
+//   { label: 'Mobile', value: 9 },
+//   { label: 'Conversion', value: 9 },
+//   { label: 'Misc', value: 21 },
+//   { label: 'Databases', value: 22 },
+//   { label: 'DevOps', value: 22 },
+//   { label: 'Javascript', value: 23 },
+//   { label: 'Languages / Frameworks', value: 25 },
+//   { label: 'Front End', value: 26 },
+//   { label: 'Content', value: 26 },
+// ]
 
 
 class App extends React.Component {
